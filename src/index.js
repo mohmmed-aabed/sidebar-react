9d348facea700f400525e10c03a435de9da7b698
Mohammed Aabed <mohmmed.aabed@gmail.com>
2021-03-03 15:22:21 +0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { AppProvider } from './context';
+
+ReactDOM.render(
+  <AppProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </AppProvider>,
+  document.getElementById('root')
+);
